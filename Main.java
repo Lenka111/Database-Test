d0120e698cb440d8acccaa43a47dffa3cf0ce571
Elena Voinu <37947819+Lenka111@users.noreply.github.com>
2020-04-20 15:12:47 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            //will create the database if it doesn't exist, otherwise connect to it
+            Connection con = DriverManager.getConnection("jdbc:sqlite:test.db");
+            Statement statement = con.createStatement();
+            statement.execute("CREATE TABLE contacts(name TEXT, phone INTEGER, email TEXT)");
+
+            //close the resource
+            //statement must be closed before we close the connection
+            statement.close();
+            //close the connection
+            con.close();
+
+        }
+        catch (SQLException e){
+            System.out.println("Something went wrong: " + e.getMessage());
+        }
+    }
+}
